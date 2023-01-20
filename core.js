44db27cd2ca534c1a06be500ae73f46a9548f261
UnholyCow <unholycow@gmail.com>
2023-01-19 21:25:23 -0600

    playRound done, just need to hook it into playGame

--- core.js
+++ core.js
@@ -3,7 +3,9 @@ const gameChoices = [
 	{ 'id': 'Rock', 'value': 0 },
 	{ 'id': 'Paper', 'value': 1 },
 	{ 'id': 'Scissors', 'value': 2 }
-]
+];
+const playerScore = 0;
+const computerScore = 0;
 
 // GENERATE AND RETURN 0/1/2 VALUES TO ASSIGN TO ROCK/PAPER/SCISSORS
 function getComputerChoice() {
@@ -23,31 +25,44 @@ function getPlayerChoice() {
 	}
 }
 
-// PLAY A SINGLE ROUND OF RPS AND RETURN THE WINNER
+// PLAY A SINGLE ROUND OF RPS AND RETURN THE RESULT
 function playRound(playerSelection = getPlayerChoice(), computerSelection = getComputerChoice()) {
-	const cpuWin = console.log('CPU wins with ' + gameChoices[computerSelection].id + '!');
-	const playerWin = console.log('Player wins with ' + gameChoices[playerSelection].id + '!');
+	const cpuWin = 'CPU wins with ' + gameChoices[computerSelection].id + '!';
+	const playerWin = 'Player wins with ' + gameChoices[playerSelection].id + '!';
+	const matchDraw = 'Draw! Player and CPU both chose ' + gameChoices[playerSelection].id + '.';
+	let result = '';
 
+	console.log('Player chose ' + gameChoices[playerSelection].id + ' (' + gameChoices[playerSelection].value + ')' + '. CPU chose ' + gameChoices[computerSelection].id + ' (' + gameChoices[computerSelection].value + ').');
 
-	if (playerSelection == computerSelection) {
-		console.log('Draw!');
-	} else if (playerSelection = 0) {
-		if (computerSelection = 1) {
-			return cpuWin;
-		} else {
-			return playerWin;
+	if (playerSelection === computerSelection) {
+		result = matchDraw;
+	} else if (playerSelection == 0) {
+		if (computerSelection == 1) {
+			result = cpuWin;
+		} else if (computerSelection == 2) {
+			result = playerWin;
 		}
-	} else if (playerSelection = 1) {
-		if (computerSelection = 0) {
-			return playerWin;
-		} else {
-			return cpuWin;
+	} else if (playerSelection == 1) {
+		if (computerSelection == 0) {
+			result = playerWin;
+		} else if (computerSelection == 2) {
+			result = cpuWin;
+		}
+	} else if (playerSelection == 2) {
+		if (computerSelection == 0) {
+			result = cpuWin;
+		} else if (computerSelection == 1) {
+			result = playerWin;
 		}
 	}
+
+	console.log(result);
 }
+
 
 // PLAY A GAME (DEFAULT 5 ROUNDS) OF RPS
 function playGame(rounds = 5) {
-	let playerScore = 0;
-	let computerScore = 0;
+	for (let r = 0; r <= rounds; r++) {
+
+	}
 }
